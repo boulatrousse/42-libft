54b222b64ab5029e237aa36048340733c3ec1393
Leo Boulatrous <lboulatr@z1r4p5.42lyon.fr>
2022-11-13 16:29:57 +0100

    3rd commit

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: lboulatr <lboulatr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 17:12:07 by lboulatr          #+#    #+#             */
-/*   Updated: 2022/11/09 08:31:13 by lboulatr         ###   ########.fr       */
+/*   Updated: 2022/11/13 16:04:27 by lboulatr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (dest == src || n == 0)
+		return (dest);
+	if (!dest && !src)
+		return (0);
 	while (i < n)
 	{
 		((char *)dest)[i] = ((char *)src)[i];
